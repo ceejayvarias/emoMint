d2d0df91389695839a1311dc6e29480cd6da740b
Andrew Kil <andrewkil86@gmail.com>
2016-08-16 20:38:59 -0700

    updating music.js

--- assets/music.js
+++ assets/music.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 function spotify() {
-	var queryURL = http://ws.spotify.com/search/1/album?q=foo
+	var queryURL = "https://api.spotify.com/v1/search?query=" + /*keyword*/ + "&offset=0&limit=20&type=album";
 
 $.ajax({url: queryURL, method: 'GET'})
   .done(function(response){
