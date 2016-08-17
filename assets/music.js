699812e10012dd7aadd6e32943b305f891c14c9e
Andrew Kil <andrewkil86@gmail.com>
2016-08-16 19:52:02 -0700

    initializing music.js

---
+++ assets/music.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+function spotify() {
+	var queryURL = http://ws.spotify.com/search/1/album?q=foo
+
+$.ajax({url: queryURL, method: 'GET'})
+  .done(function(response){
+}
+
+
+}); // closes document ready
