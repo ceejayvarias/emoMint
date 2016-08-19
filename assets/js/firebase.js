89a7e3ac92da8e144819efa815ca14b6c9e93d8d
Andrew Kil <andrewkil86@gmail.com>
2016-08-18 19:44:28 -0700

    intializing firebase

---
+++ assets/js/firebase.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCB6UoptyCirEB8kfeeiEYA1sztDXGMcOM",
+    authDomain: "emomint-a031f.firebaseapp.com",
+    databaseURL: "https://emomint-a031f.firebaseio.com",
+    storageBucket: "",
+  };
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
