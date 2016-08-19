c2f0e9ebbf572b4a9e502bc0cb24fcd93533ac8d
Guy <guypham@hotmail.com>
2016-08-18 19:25:56 -0700

    added token

--- assets/js/movie.js
+++ assets/js/movie.js
@@ -1,12 +1,8 @@
-<html>
-<head>
-	<title></title>
-</head>
-<body>
+
 
 <script src='http://code.jquery.com/jquery-2.1.3.min.js'></script>
 <script type="text/javascript">
-var randomMovieArray = ['Love Actually', 'When Harry Met Sally', 'Stepbrothers', 'The Hangover', 'Bridesmaids', 'Just Friends', 'Airplane', 'Groundhog day', 'Old School', 'Tropic Thunder'];
+var randomMovieArray = ['Love Actually', 'When Harry Met Sally', 'Stepbrothers', 'The Hangover', 'Bridesmaids', 'Just Friends', 'Airplane', 'Groundhog day', 'Old School', 'Tropic Thunder', 'Frozen'];
 
 function apiCall() {
 	var randomNumber = Math.floor((Math.random() * randomMovieArray.length + 1));
@@ -21,13 +17,6 @@ function apiCall() {
 	});
 }
 
-$('#movies').click(function{
+$('#movies').click(function(){
 	apiCall()
 });
-
-
-</script>
-
-
-</body>
-</html>
