31a90ae8c8e349c0ffeb09b2c16af9669d1da53a
Andrew Kil <andrewkil86@gmail.com>
2016-08-20 14:04:34 -0700

    updating music.js

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -1,10 +1,11 @@
 // Initialize Firebase
   var config = {
-    apiKey: "AIzaSyCB6UoptyCirEB8kfeeiEYA1sztDXGMcOM",
-    authDomain: "emomint-a031f.firebaseapp.com",
-    databaseURL: "https://emomint-a031f.firebaseio.com",
-    storageBucket: "",
-  };
+   apiKey: "AIzaSyDAW-OXo25GXGm5ZAa8DDTHrckGc_QF0Jw",
+   authDomain: "emomint-4519e.firebaseapp.com",
+   databaseURL: "https://emomint-4519e.firebaseio.com",
+   storageBucket: "emomint-4519e.appspot.com",
+ };
+
   firebase.initializeApp(config);
 
   var database = firebase.database();
--- assets/js/music.js
+++ assets/js/music.js
@@ -1,21 +1,37 @@
-$(document).ready(function(){
-function spotify() {
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDAW-OXo25GXGm5ZAa8DDTHrckGc_QF0Jw",
+    authDomain: "emomint-4519e.firebaseapp.com",
+    databaseURL: "https://emomint-4519e.firebaseio.com",
+    storageBucket: "emomint-4519e.appspot.com",
+  };
+  firebase.initializeApp(config);
+ var database = firebase.database();
 
-	var queryURL = "https://api.spotify.com/v1/search?query=" + /*keyword*/ + "&offset=0&limit=20&type=album";
+database.ref().on("child_added", function(childSnapshot, prevChildKey){
+
+  var smile = childSnapshot.val().emotion;
+
+
+var queryURL = "https://api.spotify.com/v1/search?query=" + /*keyword*/ + "&offset=0&limit=20&type=album";
 
 $.ajax({url: queryURL, method: 'GET'})
   .done(function(response){
-  	//NEED VARIABLE FOR DATA SEARCH KEY FROM FIREBASE
-  	var result = response.albums.items;
+    //NEED VARIABLE FOR DATA SEARCH KEY FROM FIREBASE
+    var result = response.albums.items;
 
-  	for(var i = 0; i < result.length; i++) {
-  		var newDiv = $('<div class="newMusic">')
-  		var newAlbum = $('<img>').attr('src', 'results[i].images[1].url');
+    for(var i = 0; i < result.length; i++) {
+      var newDiv = $('<div class="newMusic">')
+      var newAlbum = $('<img>').attr('src', 'results[i].images[1].url');
 
-  		$(newDiv).append(newAlbum);
+      $(newDiv).append(newAlbum);
 
-  		$('#music').prepend(newDiv);
-  	};
+      $('#music').prepend(newDiv);
+    };
 });
-}; // closes spotify
+
 }); // closes document ready
+}
+
+
+
