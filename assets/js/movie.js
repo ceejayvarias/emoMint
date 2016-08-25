7f5e9efc4f16fc0aef78ef69ff53c59871a7d489
Guy <guypham@hotmail.com>
2016-08-24 20:32:45 -0700

    modified the movie database

--- assets/js/movie.js
+++ assets/js/movie.js
@@ -1,7 +1,6 @@
 // Initialize Firebase
 
 var database = firebase.database();
-function apiCall() {
 
 database.ref().on('child_added', function(snap){
     snap.val().emotion;
@@ -12,29 +11,35 @@ database.ref().on('child_added', function(snap){
         }
     else if(snap.val().emotion = 'sad'){
             id = 18;
+    } else if(snap.val().emotion = 'content'){
+            id = 9648;
+    } else if(snap.val().emotion = 'angry'){
+            id = 28;
     }
-
     var queryURL = "http://api.themoviedb.org/3/discover/movie?with_genres=" + id + "&api_key=bcc2f00706a523f852a1b201ab755717";
     var movie = [];
-    var moviePosters = [];
+
     $.ajax({url: queryURL, method: 'GET'}).done(function(response) {
+      var moviePosters = {
+        movie1 : "http://image.tmdb.org/t/p/w500" + response.results[0].poster_path,
+        movie2 : "http://image.tmdb.org/t/p/w500" + response.results[1].poster_path,
+        movie3 : "http://image.tmdb.org/t/p/w500" + response.results[2].poster_path
 
-        for (var i = 0; i < 3; i++) {
+      }
+      snap.child("movies").ref.set(moviePosters);
+    //     for (var i = 0; i < 3; i++) {
+    //
+    //       var movieURL = "http://image.tmdb.org/t/p/w500" + response.results[i].poster_path;
+    //       moviePosters.push(movieURL);
+    //       console.log(response.results[i]);
+    //       console.log(response.results[i].title);
+    //       movie.push(response.results[i]);
+    // }
+    // console.log(movie);
+    });
+});
 
-          var movieURL = "http://image.tmdb.org/t/p/w500" +  response.results[i].poster_path;
-          moviePosters.push(movieURL);
-          console.log(response.results[i]);
-          console.log(response.results[i].title);
-          movie.push(response.results[i]);
-        }
-        console.log(movie);
-          });
-        });
-      };
 
-      $('#movies').click(function(){
-          apiCall();
-        });
 
 //     var image = response.Poster;
 //
