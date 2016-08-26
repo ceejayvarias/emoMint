94966d5c60b86eb42bccd217ef2f7203a942943f
Ceejay Varias <ceejayvarias@ucla.edu>
2016-08-26 05:03:06 -0700

    fixed index, randomizer, display after upload, and css

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -1,27 +1,20 @@
 // Initialize Firebase
 var database = firebase.database();
-
 database.ref().on('child_added', function(snapshot, prevChildkey){
-	// console.log(snapshot.val().emotion);
 	var key = snapshot.key;
-	var date = snapshot.val().date; //must convert later
+	var date = snapshot.val().date;
 	var convertedDate = moment(date).format("MMM DD, YYYY hh:mm a")
-	// console.log("time converting: " + convertedDate);
 	var emotion = snapshot.val().emotion;
 	var image = snapshot.val().image;
-	// console.log(music);
 
-	// var d = $('<div>');
 	var i = $('<img>');
 	var row = $('<tr>');
 	i.attr('src', image);
 	i.css('width', 50);
 	row.attr('data-date', convertedDate);
-	row.attr('data-image', image);
 	row.attr('data-emotion', emotion);
 	row.attr('data-key', key);
 	row.addClass('log');
-	// row.append(i);
 	
 	var tdImage = $('<td>');
 	tdImage.append(i);
@@ -35,42 +28,53 @@ database.ref().on('child_added', function(snapshot, prevChildkey){
 	$('#table-body').append(row);
 });
 
+//function that displays the music and movies
+function setEntertainment(arr, type){
+		for (var j = 0; j < arr.length; j++) {
+			var li = $('<li>');
+			var i = $('<img>');
+			var a = $('<a>');
+			i.addClass('thumbnail');
+			i.attr('src', arr[j]);
+			a.attr('href', arr[j]);
+			a.attr('target', '_blank');
+			a.append(i);
+			li.append(a);
+			if (type == 'movie') {
+				$('#poster').append(li);
+			}
+			else if(type == 'music') {
+				$('#cover').append(li);
+			}
+		}
+}
+
+//on click for logs
 $(document).on('click', '.log', function(){
 	var key = $(this).data('key');
 	var movie = [], music = [];
-	var element = this;
 	database.ref().child(key).once('value', function(snapshot){
 		var movieRef = snapshot.child("movies");
 		var musicRef = snapshot.child("albums");
+		var journal = snapshot.child("journal").val();
+		var image = snapshot.child("image").val();
+		var emotion = snapshot.child("emotion").val();
 		music.push(musicRef.val().album1);
 		music.push(musicRef.val().album2);
 		music.push(musicRef.val().album3);
+		music.push(musicRef.val().album4);
 		movie.push(movieRef.val().movie1);
 		movie.push(movieRef.val().movie2);
 		movie.push(movieRef.val().movie3);
+		movie.push(movieRef.val().movie4);
+		$('#personPhoto').empty();
+		var i = $('<img>');
+		$('#personPhoto').append(i.attr('src', image));
+		$('#entry').html("Emotion at the time: " + emotion + "<br>" + journal);
 		$('#poster').empty();
 		$('#cover').empty();
-		$('#selfie').empty();
-		// console.log(this);
-		var i = $('<img>');
-		$('#selfie').append(i.attr('src', $(element).data('image')));
+		setEntertainment(movie, 'movie');
+		setEntertainment(music, 'music');
 
-		//set movies
-		for (var j = 0; j < 3; j++) {
-			var li = $('<li>');
-			var i = $('<img>');
-			var film = "movie" + j;
-			li.append(i.attr('src', movie[j]));
-			$('#poster').append(li);
-		}
-
-		//set music
-		for (var j = 0; j < 3; j++) {
-			var li = $('<li>');
-			var i = $('<img>');
-			li.append(i.attr('src', music[j]));
-			$('#cover').append(li);
-		}
-	})
-
-})
+	});
+});
--- assets/js/movie.js
+++ assets/js/movie.js
@@ -2,34 +2,50 @@
 
 var database = firebase.database();
 
-console.log(sessionStorage.getItem('refresh'));
+//grabs the last child that was added and sets movie suggestions
 database.ref().orderByChild('date').limitToLast(1).on('child_added', function(snap, prevChildKey){
     var id;
-    if(snap.val().emotion == 'happy') {
+    var emotion = snap.val().emotion;
+
+    if(emotion == 'happy') {
     	id = 35;
     }
-    else if(snap.val().emotion == 'sad'){
+    else if(emotion == 'sad'){
         id = 18;
-    } else if(snap.val().emotion == 'content'){
+    } else if(emotion == 'content'){
 		id = 9648;
-    } else if(snap.val().emotion == 'angry'){
+    } else if(emotion == 'angry'){
 		id = 28;
     }
-	var queryURL = "http://api.themoviedb.org/3/discover/movie?with_genres=" + id + "&api_key=bcc2f00706a523f852a1b201ab755717";
-	var movie = [];
-	if (sessionStorage.getItem('refresh') === "true") {
-		$.ajax({url: queryURL, method: 'GET'}).done(function(response) {
-			var moviePosters = {
-		        movie1 : "http://image.tmdb.org/t/p/w500" + response.results[getRandNum(0, 6)].poster_path,
-		        movie2 : "http://image.tmdb.org/t/p/w500" + response.results[getRandNum(7, 13)].poster_path,
-		        movie3 : "http://image.tmdb.org/t/p/w500" + response.results[getRandNum(14, 20)].poster_path
-			}
 
-		    // if (snap.child("movies").val() == null) {
-		    	snap.child("movies").ref.set(moviePosters);
-		    // }
-		    });
-		
-	}
-	sessionStorage.setItem('refresh', "false");
+	var queryURL = "http://api.themoviedb.org/3/discover/movie?with_genres=" + id + "&api_key=bcc2f00706a523f852a1b201ab755717";
+	
+	$.ajax({url: queryURL, method: 'GET'}).done(function(response) {
+		var results = response.results;
+		var moviePosters = {
+	        movie1 : "http://image.tmdb.org/t/p/w500" + results[getRandNum(0,4)].poster_path,
+	        movie2 : "http://image.tmdb.org/t/p/w500" + results[getRandNum(5,9)].poster_path,
+	        movie3 : "http://image.tmdb.org/t/p/w500" + results[getRandNum(10,14)].poster_path,
+	        movie4 : "http://image.tmdb.org/t/p/w500" + results[getRandNum(15,20)].poster_path
+		}
+
+		if (snap.child("movieuploaded").val()) {
+	    	snap.child("movies").ref.set(moviePosters);
+	    	snap.child("movieuploaded").ref.set(false);
+	    	$('#poster').empty();
+		    for (var key in moviePosters) {
+		      var li = $('<li>');
+		      var i = $('<img>');
+		      	var a = $('<a>');
+				i.addClass('thumbnail');
+				i.attr('src', moviePosters[key]);
+				a.append(i);
+				a.attr('href', moviePosters[key]);
+				a.attr('target', '_blank');
+				li.append(a);
+		      $('#poster').append(li);
+		    }
+	    }
+	    
+	});	
 });
